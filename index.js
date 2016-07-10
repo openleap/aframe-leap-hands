0a76af88dd9a8ba93695d0e2d119eae4246cb462
Don McCurdy <don.r.mccurdy@gmail.com>
2016-07-09 22:37:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 module.exports = {
   'system': require('./src/leap-system'),
   'leap-hand': require('./src/leap-hand'),
-  'leap-hand-body': require('./src/leap-hand-body'),
   registerAll: function () {
     AFRAME.registerSystem('leap', this.system);
     AFRAME.registerComponent('leap-hand', this['leap-hand']);
-    AFRAME.registerComponent('leap-hand-body', this['leap-hand-body']);
   }
 };
