5e5ee47fd5fc91993cc57a590e85fc0827774877
Don McCurdy <don.r.mccurdy@gmail.com>
2016-08-17 22:24:23 -0400

    Add dist/ builds (fixes #8)

--- browser.js
+++ browser.js
@@ -1,6 +1,1 @@
-window.Leap = require('leapjs');
-window.AFRAME = require('aframe');
-window.AFRAME.extras = require('aframe-extras');
-
-window.AFRAME.extras.registerAll();
 require('./').registerAll();
---
+++ examples/index.js
@@ -0,0 +1,6 @@
+window.Leap = require('leapjs');
+window.AFRAME = require('aframe');
+window.AFRAME.extras = require('aframe-extras');
+
+window.AFRAME.extras.registerAll();
+require('../').registerAll();
