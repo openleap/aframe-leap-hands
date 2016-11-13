760dd8e9f6756646920f012f1cb59c7b8611ee61
Don McCurdy <don.r.mccurdy@gmail.com>
2016-11-13 16:57:29 -0500

    Add transform options to leap system. (#14)
    * Allows developers to disable 'vr' mode.
    * Exposes 'position', 'quaternion', and 'scale' via schema.
    
    Fixes #11.
    Fixes #13.

--- src/leap-system.js
+++ src/leap-system.js
@@ -1,21 +1,42 @@
 var Leap = require('leapjs'),
     transform = require('../lib/leap.transform.js');
 
+// Defaults from leap.transform.js.
+var DEFAULT_SCALE = 0.001;
+var DEFAULT_POSITION = new THREE.Vector3();
+var DEFAULT_QUATERNION = new THREE.Quaternion();
+
 Leap.Controller.plugin('transform', transform);
-Leap.plugin('aframeSystem', function(options) {
-  options = options || {};
-  this.use('transform', {vr: true});
-  return {hand: function () {}};
-});
 
 /**
  * Leap Motion system for A-Frame.
  */
 module.exports = {
+  schema: {
+    vr: {default: true},
+    scale: {default: DEFAULT_SCALE},
+    position: {
+      type: 'vec3',
+      default: {
+        x: DEFAULT_POSITION.x,
+        y: DEFAULT_POSITION.y,
+        z: DEFAULT_POSITION.z,
+      }
+    },
+    quaternion: {
+      type: 'vec4',
+      default: {
+        x: DEFAULT_QUATERNION.x,
+        y: DEFAULT_QUATERNION.y,
+        z: DEFAULT_QUATERNION.z,
+        w: DEFAULT_QUATERNION.w
+      }
+    }
+  },
+
   init: function () {
     this.controller = Leap.loop()
-      .setOptimizeHMD(true)
-      .use('aframeSystem');
+      .use('transform', this.data);
   },
 
   getFrame: function () {
